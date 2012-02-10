d4a9e4461f1b8aead17fc55fe32f63ca3df4ca29
Daisuke Miyamoto <dai.0304@gmail.com>
2012-02-10 18:02:28 +0900

    add CalendarRangeValidator

---
+++ src/main/java/jp/xet/baseunits/wicket/CalendarRangeValidator.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2011 Daisuke Miyamoto. (http://d.hatena.ne.jp/daisuke-m)
+ * Created on 2012/02/10
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package jp.xet.baseunits.wicket;
+
+import jp.xet.baseunits.time.CalendarDate;
+
+import org.apache.commons.lang.Validate;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.form.validation.AbstractFormValidator;
+
+/**
+ * TODO for daisuke
+ */
+@SuppressWarnings("serial")
+public class CalendarRangeValidator extends AbstractFormValidator {
+	
+	private final FormComponent<CalendarDate> from;
+	
+	private final FormComponent<CalendarDate> to;
+	
+	
+	/**
+	 * インスタンスを生成する。
+	 * 
+	 * @param from 自
+	 * @param to 至
+	 */
+	public CalendarRangeValidator(FormComponent<CalendarDate> from, FormComponent<CalendarDate> to) {
+		Validate.notNull(from);
+		Validate.notNull(to);
+		this.from = from;
+		this.to = to;
+	}
+	
+	@Override
+	public FormComponent<?>[] getDependentFormComponents() {
+		return new FormComponent[] {
+			from,
+			to
+		};
+	}
+	
+	@Override
+	public void validate(Form<?> form) {
+		CalendarDate fromDate = from.getConvertedInput();
+		CalendarDate toDate = to.getConvertedInput();
+		if (fromDate.isAfter(toDate)) {
+			error(from);
+		}
+	}
+}
