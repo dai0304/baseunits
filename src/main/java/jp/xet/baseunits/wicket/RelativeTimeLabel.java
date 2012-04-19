2d853274e83efdeef47bb7de8218c24d8c67e608
Daisuke Miyamoto <dai.0304@gmail.com>
2012-04-19 14:36:59 +0900

    add RelativeTimeLabel

---
+++ src/main/java/jp/xet/baseunits/wicket/RelativeTimeLabel.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2011-2012 Cloudstudy, Inc..
+ * Created on 2012/04/19
+ * 
+ * All rights reserved.
+ */
+package jp.xet.baseunits.wicket;
+
+import java.util.Locale;
+import java.util.TimeZone;
+
+import jp.xet.baseunits.time.TimePoint;
+import jp.xet.baseunits.time.formatter.Icu4jRelativeTimePointFormatter;
+import jp.xet.baseunits.time.formatter.RelativeTimePointFormatter;
+import jp.xet.baseunits.time.formatter.RelativeTimePointFormatter.FallbackConfig;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.IConverter;
+
+/**
+ * TODO for daisuke
+ * 
+ * @since 2.2
+ * @version $Id$
+ * @author daisuke
+ */
+@SuppressWarnings("serial")
+public class RelativeTimeLabel extends GenericLabel<TimePoint> {
+	
+	private final RelativeTimePointFormatter formatter;
+	
+	
+	/**
+	 * インスタンスを生成する。
+	 * 
+	 * @param id The non-null id of this component
+	 * @param config {@link FallbackConfig}
+	 * @param timeZone {@link TimeZone}
+	 */
+	public RelativeTimeLabel(String id, FallbackConfig config, TimeZone timeZone) {
+		super(id);
+		formatter = new Icu4jRelativeTimePointFormatter(config, timeZone);
+	}
+	
+	/**
+	 * インスタンスを生成する。
+	 * 
+	 * @param id The non-null id of this component
+	 * @param model The component's model
+	 * @param config {@link FallbackConfig}
+	 * @param timeZone {@link TimeZone}
+	 */
+	public RelativeTimeLabel(String id, IModel<TimePoint> model, FallbackConfig config, TimeZone timeZone) {
+		super(id, model);
+		formatter = new Icu4jRelativeTimePointFormatter(config, timeZone);
+	}
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public <C>IConverter<C> getConverter(Class<C> type) {
+		if (type == TimePoint.class) {
+			return (IConverter<C>) new IConverter<TimePoint>() {
+				
+				@Override
+				public TimePoint convertToObject(String value, Locale locale) {
+					throw new UnsupportedOperationException();
+				}
+				
+				@Override
+				public String convertToString(TimePoint value, Locale locale) {
+					return formatter.format(value, locale);
+				}
+			};
+		}
+		return super.getConverter(type);
+	}
+}
