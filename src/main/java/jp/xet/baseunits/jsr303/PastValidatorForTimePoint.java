29576dd8a788451553f005f94eda272ece7badfa
Daisuke Miyamoto <dai.0304@gmail.com>
2012-11-20 18:21:21 +0900

    add JSR-303 validator implemations

---
+++ src/main/java/jp/xet/baseunits/jsr303/PastValidatorForTimePoint.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011 Daisuke Miyamoto. (http://d.hatena.ne.jp/daisuke-m)
+ * Created on 2012/11/14
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package jp.xet.baseunits.jsr303;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.Past;
+
+import jp.xet.baseunits.time.TimePoint;
+import jp.xet.baseunits.timeutil.Clock;
+
+/**
+ * TODO for daisuke
+ */
+public class PastValidatorForTimePoint implements ConstraintValidator<Past, TimePoint> {
+	
+	@Override
+	public void initialize(Past constraintAnnotation) {
+	}
+	
+	@Override
+	public boolean isValid(TimePoint value, ConstraintValidatorContext context) {
+		// null values are valid
+		if (value == null) {
+			return true;
+		}
+		return value.isBefore(Clock.now());
+	}
+}
